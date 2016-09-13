2ec18472ebe8bc0e0bbf7c667ae1ef38128d722d
dartess@mail.ru <dartess@mail.ru>
2016-09-14 00:53:21 +0400

    added test "basic with leading ?"

--- test/url-search-params.js
+++ test/url-search-params.js
@@ -29,6 +29,26 @@ wru.test([
       wru.assert('usp to string works as expected', usp.toString() === 'b=2');
     }
   }, {
+    name: 'basics with leading "?"',
+    test: function () {
+      var usp = new URLSearchParams('?a=1&b=2');
+      wru.assert('has keys', usp.has('a') && usp.has('b'));
+      wru.assert('a returns right value', usp.get('a') === '1');
+      wru.assert('b returns right value', usp.get('b') === '2');
+      wru.assert('a getAll returns right value', usp.getAll('a').join(',') === '1');
+      wru.assert('b getAll returns right value', usp.getAll('b').join(',') === '2');
+      usp.append('a', '3');
+      wru.assert('append adds values', usp.getAll('a').join(',') === '1,3');
+      wru.assert('append preserves get', usp.get('a') === '1');
+      wru.assert('append does not affect others', usp.getAll('b').join(',') === '2');
+      usp.set('a', '4');
+      wru.assert('set overwrites known values', usp.getAll('a').join(',') === '4');
+      usp['delete']('a');
+      wru.assert('usp can delete', usp.has('a') === false);
+      wru.assert('usp can return null', usp.get('a') === null);
+      wru.assert('usp to string works as expected', usp.toString() === 'b=2');
+    }
+  }, {
     name: 'edge cases',
     test: function () {
       var usp = new URLSearchParams();
